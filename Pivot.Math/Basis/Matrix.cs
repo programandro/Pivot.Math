6b6d4a768555962990ddfe562aeb11f84be16eff
Alejandro Rodriguez <alejandro@dycoder.com>
2018-04-21 09:53:38 -0400

    Add project files.

---
+++ Pivot.Math/Basis/Matrix.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pivot.Math.Basis
+{
+    public class Matrix : ICloneable
+    {
+        protected double[,] _values;
+        protected int _rows;
+        protected int _columns;
+
+        public Matrix(int rows, int columns)
+        {
+            _values = new double[rows, columns];
+            _rows = rows;
+            _columns = columns;
+        }
+
+        public Matrix(int size) : this(size, size) { }
+
+        public static Matrix NullMatrix(int rows, int columns)
+        {
+            var matrix = new Matrix(rows, columns);
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < columns; j++)
+                    matrix[i, i] = 0;
+            return matrix;
+        }
+
+        public static Matrix Identity(int rows, int columns)
+        {
+            var identity = new Matrix(rows, columns);
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < columns; j++)
+                    if (i == j) identity[i, j] = 1;
+                    else identity[i, j] = 0;
+            return identity;
+        }
+
+        public static Matrix IdentityMatrix(int size)
+            => Identity(size, size);
+
+        public virtual double this[int rowIndex, int columnIndex]
+        {
+            get => _values[rowIndex, columnIndex];
+            set => _values[rowIndex, columnIndex] = value;
+        }
+
+        public virtual int Rows => _rows;
+
+        public virtual int Columns => _columns;
+
+        public static Matrix operator /(Matrix matrix, double constant)
+            => matrix * (1 / constant);
+
+        public static Matrix operator -(Matrix matrix)
+            => matrix * (-1);
+
+        #region ICloneable Members
+
+        public virtual object Clone()
+        {
+            var matrix = new Matrix(_rows, _columns);
+            for (int i = 0; i < _rows; i++)
+                for (int j = 0; j < _columns; j++)
+                    matrix[i, j] = this[i, j];
+            return matrix;
+        }
+
+        #endregion
+
+        public virtual void InterchangeRows(int index1, int index2)
+        {
+            double temp;
+            for (int i = 0; i < this._columns; i++)
+            {
+                temp = this[index1, i];
+                this[index1, i] = this[index2, i];
+                this[index2, i] = temp;
+            }
+        }
+
+        public virtual void InterchangeColumns(int index1, int index2)
+        {
+            double temp;
+            for (int i = 0; i < _rows; i++)
+            {
+                temp = this[i, index1];
+                this[i, index1] = this[i, index2];
+                this[i, index2] = temp;
+            }
+        }
+
+        public virtual bool IsSquare => Rows == Columns;
+
+        public virtual Matrix Transpose()
+        {
+            var matrix = new Matrix(_columns, _rows);
+            for (int i = 0; i < _rows; i++)
+                for (int j = 0; j < _columns; j++)
+                    matrix[i, j] = this[j, i];
+            return matrix;
+        }
+
+        public static Matrix operator *(Matrix matrix, double constant)
+        {
+            int col = matrix.Columns, r = matrix.Rows;
+            var result = new Matrix(col, r);
+            for (int i = 0; i < r; i++)
+                for (int j = 0; j < col; j++)
+                    result[i, j] = matrix[i, j] * constant;
+            return result;
+        }
+
+        public static Matrix operator *(double constant, Matrix matrix)
+            => matrix * constant;
+
+        public static Vector operator *(Matrix matrix, Vector vector)
+        {
+            if (vector.Length != matrix.Columns)
+                throw new Exception("Can't multiply!");
+            Vector result = new Vector(matrix.Columns);
+            for (int i = 0; i < matrix.Rows; i++)
+            {
+                double s = 0;
+                for (int j = 0; j < matrix.Columns; j++)
+                    s += matrix[i, j] * vector[j];
+                result[i] = s;
+            }
+            return result;
+        }
+
+        public static Matrix operator +(Matrix matrix1, Matrix matrix2)
+        {
+            int constant = matrix1.Columns, r = matrix1.Rows;
+            if ((constant != matrix2.Columns) || (r != matrix2.Rows))
+                throw new Exception("Can't sum!");
+            Matrix sum = new Matrix(constant, r);
+            for (int i = 0; i < r; i++)
+                for (int j = 0; j < constant; j++)
+                    sum[i, j] = matrix1[i, j] + matrix2[i, j];
+            return sum;
+        }
+
+        public static Matrix operator +(Matrix matrix, Vector vector)
+        {
+            int constant = matrix.Columns, r = matrix.Rows;
+            if (((constant > 1) && (r > 1)) || ((r != vector.Length) && (constant != vector.Length)))
+                throw new Exception("Can't sum");
+            Matrix result = new Matrix(r, constant);
+            if (constant == 1)
+            {
+                for (int i = 0; i < vector.Length; i++)
+                    result[i, 0] = matrix[i, 0] + vector[i];
+            }
+            else
+            {
+                for (int i = 0; i < vector.Length; i++)
+                    result[0, i] = matrix[0, i] + vector[i];
+            }
+            return result;
+        }
+
+        public static Matrix operator -(Matrix matrix1, Matrix matrix2)
+        {
+            int constant = matrix1.Columns, r = matrix1.Rows;
+            if ((constant != matrix2.Columns) || (r != matrix2.Rows))
+                throw new Exception("Can't rest!");
+            Matrix rest = new Matrix(r, constant);
+            for (int i = 0; i < r; i++)
+                for (int j = 0; j < constant; j++)
+                    rest[i, j] = matrix1[i, j] - matrix2[i, j];
+            return rest;
+        }
+
+        public static Matrix operator -(Matrix matrix, Vector vector)
+        {
+            int constant = matrix.Columns, r = matrix.Rows;
+            if (((constant > 1) && (r > 1)) || ((r != vector.Length) && (constant != vector.Length)))
+                throw new Exception("Can't sum");
+            Matrix result = new Matrix(r, constant);
+            if (constant == 1)
+            {
+                for (int i = 0; i < vector.Length; i++)
+                    result[i, 0] = matrix[i, 0] - vector[i];
+            }
+            else
+            {
+                for (int i = 0; i < vector.Length; i++)
+                    result[0, i] = matrix[0, i] - vector[i];
+            }
+            return result;
+        }
+
+
+        public static Matrix operator *(Matrix matrix1, Matrix matrix2)
+        {
+            int c1 = matrix1.Columns, r1 = matrix1.Rows, c2 = matrix2.Columns, r2 = matrix2.Rows;
+            if (c1 != r2)
+                throw new Exception("Can't multilply!");
+            Matrix mult = new Matrix(r1, c2);
+            for (int i = 0; i < r1; i++)
+                for (int k = 0; k < c2; k++)
+                {
+                    double s = 0;
+                    for (int j = 0; j < c1; j++)
+                        s += matrix1[i, j] * matrix2[j, k];
+                    mult[i, k] = s;
+                }
+            return mult;
+        }
+
+        public virtual Vector RowToVector(int index)
+        {
+            Vector result = new Vector(_columns);
+            for (int i = 0; i < _columns; i++)
+                result[i] = this[index, i];
+            return result;
+        }
+
+        public virtual Vector ColumnToVector(int index)
+        {
+            Vector result = new Vector(_rows);
+            for (int i = 0; i < _rows; i++)
+                result[i] = this[i, index];
+            return result;
+        }
+
+        public virtual void ChangeRow(int index, Vector vector)
+        {
+            if (vector.Length != _columns)
+                throw new Exception("Invalid vector");
+            for (int i = 0; i < _columns; i++)
+                this[index, i] = vector[i];
+        }
+
+        public virtual void ChangeColumn(int index, Vector vector)
+        {
+            if (vector.Length != _rows)
+                throw new Exception("Invalid vector!");
+            for (int i = 0; i < _rows; i++)
+                this[i, index] = vector[i];
+        }
+
+        /// <summary>
+        /// Multiply a source row by a constant and adds the result to a destination row
+        /// </summary>
+        public virtual void ModifyRows(int destIndex, int sourceIndex, double constant)
+        {
+            for (int i = 0; i < _columns; i++)
+                this[destIndex, i] = this[sourceIndex, i] * constant;
+        }
+
+        /// <summary>
+        /// Multiply a source column by a constant and adds the result to a destination column
+        /// </summary>
+        public virtual void ModifyColumns(int destIndex, int sourceIndex, double constant)
+        {
+            for (int i = 0; i < _rows; i++)
+                this[i, destIndex] = this[i, sourceIndex] * constant;
+        }
+
+        public virtual void SumRows(int sourceIndex, int destIndex)
+        {
+            ModifyRows(destIndex, sourceIndex, 1);
+        }
+
+        public virtual void RestRows(int sourceIndex, int destIndex)
+        {
+            ModifyRows(destIndex, sourceIndex, -1);
+        }
+
+        public virtual void SumColumns(int sourceIndex, int destIndex)
+        {
+            ModifyColumns(destIndex, sourceIndex, 1);
+        }
+
+        public virtual void RestColumns(int sourceIndex, int destIndex)
+        {
+            ModifyColumns(destIndex, sourceIndex, -1);
+        }
+
+        public virtual Matrix Minor(int excludeRow, int excludeColumn)
+        {
+            Matrix matrix = new Matrix(_rows - 1, _columns - 1);
+            int ki = 0, kj = 0;
+            for (int i = 0; i < _rows - 1; i++)
+            {
+                if (i == excludeRow) ki = 1;
+                for (int j = 0; j < _columns - 1; j++)
+                {
+                    if (j >= excludeColumn) kj = 1; else kj = 0;
+                    matrix[i, j] = this[i + ki, j + kj];
+                }
+            }
+            return matrix;
+        }
+
+        public virtual Matrix PrincipalMinor(int size)
+        {
+            if ((size > _columns) || (size > _rows))
+                throw new Exception("Invalid minor!");
+            Matrix matrix = new Matrix(size, size);
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    matrix[i, j] = this[i, j];
+            return matrix;
+        }
+
+        public override string ToString()
+        {
+            string s = "( ";
+            for (int i = 0; i < _rows - 1; i++)
+                s += RowToVector(i).ToString() + " , ";
+            s += RowToVector(_rows - 1).ToString() + " )";
+            return s;
+        }
+
+        public virtual Matrix PermuteRows(params int[] permutationIndexs)
+        {
+            Matrix matrix = new Matrix(_rows, _columns);
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _columns; j++)
+                {
+                    matrix[i, j] = this[permutationIndexs[i], j];
+                }
+            }
+            return matrix;
+        }
+
+        public virtual Matrix PermuteColumns(params int[] permutationIndexs)
+        {
+            Matrix matrix = new Matrix(_rows, Columns);
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _columns; j++)
+                {
+                    matrix[i, j] = this[i, permutationIndexs[j]];
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Pivot.Math/Basis/Vector.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pivot.Math.Basis
+{
+    public class Vector : ICloneable
+    {
+        private int _length;
+        private double[] _values;
+
+        public Vector(int length)
+        {
+            if (length <= 0)
+                throw new Exception("Length less than or iqual zero!");
+
+            _values = Enumerable.Repeat<double>(0, _length).ToArray();
+        }
+
+        public Vector(params double[] values)
+        {
+            _length = values.Length;
+            _values = new double[_length];
+            for (var i = 0; i < _length; i++)
+                _values[i] = values[i];
+        }
+
+        public static Vector Parse(string vector)
+        {
+            if ((vector[0] != '(') || (vector[vector.Length - 1] != ')')) throw new ArgumentException("Bad string parsing!");
+            string tvector = vector.Substring(1, vector.Length - 2);
+            string[] v = tvector.Split(',');
+            int n = v.Length;
+            Vector result = new Vector(n);
+            for (int i = 0; i < n; i++)
+                result[i] = double.Parse(v[i]);
+            return result;
+        }
+
+        public override string ToString()
+        {
+            string result = "(";
+            for (int i = 0; i < _length - 1; i++)
+                result = result + _values[i].ToString() + ", ";
+            result = result + _values[_length - 1].ToString() + ")";
+            return result;
+        }
+
+        public static Vector NullVector(int length)
+            => new Vector(length);
+
+        public virtual double this[int index]
+        {
+            get => _values[index];
+            set => _values[index] = value;
+        }
+
+        public virtual int Length => _length;
+
+        public static Vector CanonicVector(int index, int length)
+        {
+            var result = NullVector(length);
+            result[index] = 1;
+            return result;
+        }
+
+        public static Vector operator *(Vector vector, double constant)
+        {
+            Vector mult = new Vector(vector.Length);
+            for (int i = 0; i < vector.Length; i++)
+                mult[i] = vector[i] * constant;
+            return mult;
+        }
+
+        public static Vector operator *(double constant, Vector vector)
+        {
+            Vector mult = new Vector(vector.Length);
+            for (int i = 0; i < vector.Length; i++)
+                mult[i] = vector[i] * constant;
+            return mult;
+        }
+
+        public static Vector operator -(Vector vector1, Vector vector2)
+        {
+            if (vector1.Length != vector2.Length)
+                throw new Exception("Can't sum different size vectors");
+            var result = new Vector(vector1.Length);
+            for (int i = 0; i < vector1.Length; i++)
+                result[i] = vector1[i] - vector2[i];
+            return result;
+        }
+
+        public static Vector operator -(Vector vector)
+            => vector * (-1);
+
+        public virtual Vector PlusMult(double constant, Vector vector)
+            => this + (vector * constant);
+
+        public static double operator *(Vector vector1, Vector vector2)
+        {
+            double sum = 0;
+            for (int i = 0; i < vector1.Length; i++)
+                sum += vector1[i] * vector2[i];
+            return sum;
+        }
+
+        public virtual Matrix MultToMatrix(Vector vector)
+        {
+            var result = new Matrix(_length, vector.Length);
+            for (int i = 0; i < _length; i++)
+                for (int j = 0; j < vector.Length; j++)
+                    result[i, j] = this[i] * vector[j];
+            return result;
+        }
+
+        public virtual Matrix ToMatrix(bool horizontal)
+        {
+            Matrix M;
+            if (horizontal)
+                M = new Matrix(1, _length);
+            else M = new Matrix(_length, 1);
+            for (int i = 0; i < _length; i++)
+                if (horizontal)
+                    M[0, i] = this[i];
+                else M[i, 0] = this[i];
+            return M;
+        }
+
+        public static Vector operator *(Vector vector, Matrix matrix)
+        {
+            if (matrix.Rows != vector.Length)
+                throw new Exception("Can't multiply!");
+            Vector result = new Vector(matrix.Columns);
+            for (int i = 0; i < matrix.Columns; i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < vector.Length; j++)
+                    sum += vector[j] * matrix[j, i];
+                result[i] = sum;
+            }
+            return result;
+        }
+
+        public static Vector operator +(Vector vector1, Vector vector2)
+        {
+            if (vector1.Length != vector2.Length)
+                throw new Exception("Cant`n sum two vectors whit diferent dimensions");
+            Vector sum = new Vector(vector1.Length);
+            for (int i = 0; i < vector1.Length; i++)
+                sum[i] = vector1[i] + vector2[i];
+            return sum;
+        }
+
+        public static Vector operator /(Vector vector, double constant)
+            => vector * (1 / constant);
+
+        public virtual Vector Concat(Vector vector)
+        {
+            Vector result = new Vector(_length + vector.Length);
+            for (int i = 0; i < _length; i++)
+                result[i] = _values[i];
+            for (int i = _length; i < vector.Length; i++)
+                result[i] = vector[i - _length];
+            return result;
+        }
+
+        public virtual Vector SubVector(int from, int to)
+        {
+            Vector result = new Vector(to - from + 1);
+            for (int i = from; i <= to; i++)
+                result[i - from] = _values[i];
+            return result;
+        }
+
+        #region ICloneable Members
+
+        /// <summary>
+        /// Clona el vector
+        /// </summary>
+        /// <returns>Un vector con los mismos valores que el actual</returns>
+        public object Clone()
+        {
+            Vector V = new Vector(_length);
+            for (int i = 0; i < _length; i++)
+                V[i] = _values[i];
+            return V;
+        }
+
+        #endregion
+    }
+}
